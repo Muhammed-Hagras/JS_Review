3e270874257a42613ce3d3c70aca75a18915a0fb
Muhammed Hagras <muhammedhagras237@gmail.com>
2025-07-29 06:39:15 +0300

    Calculate area of circle

---
+++ main.js
@@ -0,0 +1,9 @@
+let d = prompt("What is the diameter of the circle?");
+
+let r = parseInt(d) / 2;
+
+// console.log(r)
+
+let area = Math.PI * r * r;
+
+console.log(`The area of the circle is ${area.toFixed(2)}`);
