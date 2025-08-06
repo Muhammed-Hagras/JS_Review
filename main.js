0e8da7a19585bcc60acb11f6e6d4b94197e54d4e
Muhammed Hagras <muhammedhagras237@gmail.com>
2025-08-06 08:16:14 +0300

    LeaderBoard sudents Task

--- main.js
+++ main.js
@@ -46,37 +46,109 @@
 // }
 
 /******* Statement *******/
-function magic(s) {
-  let vowels = "aeiou";
-  for (let i = 0; i < s.length; i++) {
-    if (vowels.includes(s[i])) {
-      s = s.replace(s[i], s[i].toUpperCase());
-    }
-  }
-  s = s.split("").reverse().join("");
+// function magic(s) {
+//   let vowels = "aeiou";
+//   for (let i = 0; i < s.length; i++) {
+//     if (vowels.includes(s[i])) {
+//       s = s.replace(s[i], s[i].toUpperCase());
+//     }
+//   }
+//   s = s.split("").reverse().join("");
 
-  let lastChar = s[s.length - 1];
-  // if (
-  //   lastChar === "a" ||
-  //   lastChar === "e" ||
-  //   lastChar === "i" ||
-  //   lastChar === "o" ||
-  //   lastChar === "u"
-  // ) {
-  //   s = s.slice(0, -1);
-  // }
+//   let lastChar = s[s.length - 1];
+// if (
+//   lastChar === "a" ||
+//   lastChar === "e" ||
+//   lastChar === "i" ||
+//   lastChar === "o" ||
+//   lastChar === "u"
+// ) {
+//   s = s.slice(0, -1);
+// }
 
-  for (let i = 0; i < vowels.length; i++) {
-    if (lastChar !== vowels[i]) {
-      s = s.slice(0, -1);
-      break;
-    }
-  }
-  //   console.log(lastChar);
+//   for (let i = 0; i < vowels.length; i++) {
+//     if (lastChar !== vowels[i]) {
+//       s = s.slice(0, -1);
+//       break;
+//     }
+//   }
+//   //   console.log(lastChar);
 
-  s = s.split("").join("-");
+//   s = s.split("").join("-");
 
-  return s;
-}
+//   return s;
+// }
 
-console.log(magic("hellothere"));
+// console.log(magic("hellothere"));
+
+const students = [
+  {
+    name: "Ali",
+    class: "3rd",
+    grades: {
+      math: 20,
+      physics: 25,
+      programming: 19,
+      english: 22,
+    },
+  },
+  {
+    name: "3bkreem",
+    class: "3rd",
+    grades: {
+      math: 25,
+      physics: 25,
+      programming: 25,
+      english: 25,
+    },
+  },
+
+  {
+    name: "Samer",
+    class: "3rd",
+    grades: {
+      math: 10,
+      physics: 15,
+      programming: 12,
+      english: 19,
+    },
+  },
+
+  {
+    name: "Ahmed",
+    class: "3rd",
+    grades: {
+      math: 20,
+      physics: 20,
+      programming: 20,
+      english: 20,
+    },
+  },
+
+  {
+    name: "Elgoker",
+    class: "3rd",
+    grades: {
+      math: 23,
+      physics: 25,
+      programming: 19,
+      english: 22,
+    },
+  },
+];
+
+const getLeaderBoard = (students) => {
+  let getLeaderBoard = students;
+  return getLeaderBoard
+    .map((student) => {
+      const totalGrades = Object.values(student.grades).reduce(
+        (acc, grade) => acc + grade,
+        0
+      );
+      return { ...student, totalGrades };
+    })
+    .filter((student) => student.totalGrades >= 85)
+    .sort((a, b) => b.totalGrades - a.totalGrades);
+};
+
+console.log(getLeaderBoard(students));
