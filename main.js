86b8bfa401030d0a97d05a8119407faedb3c4c54
Muhammed Hagras <muhammedhagras237@gmail.com>
2025-08-11 04:03:17 +0300

    football betting app

--- main.js
+++ main.js
@@ -81,74 +81,163 @@
 
 // console.log(magic("hellothere"));
 
-const students = [
-  {
-    name: "Ali",
-    class: "3rd",
-    grades: {
-      math: 20,
-      physics: 25,
-      programming: 19,
-      english: 22,
-    },
-  },
-  {
-    name: "3bkreem",
-    class: "3rd",
-    grades: {
-      math: 25,
-      physics: 25,
-      programming: 25,
-      english: 25,
-    },
-  },
+// const students = [
+//   {
+//     name: "Ali",
+//     class: "3rd",
+//     grades: {
+//       math: 20,
+//       physics: 25,
+//       programming: 19,
+//       english: 22,
+//     },
+//   },
+//   {
+//     name: "3bkreem",
+//     class: "3rd",
+//     grades: {
+//       math: 25,
+//       physics: 25,
+//       programming: 25,
+//       english: 25,
+//     },
+//   },
 
-  {
-    name: "Samer",
-    class: "3rd",
-    grades: {
-      math: 10,
-      physics: 15,
-      programming: 12,
-      english: 19,
-    },
-  },
+//   {
+//     name: "Samer",
+//     class: "3rd",
+//     grades: {
+//       math: 10,
+//       physics: 15,
+//       programming: 12,
+//       english: 19,
+//     },
+//   },
 
-  {
-    name: "Ahmed",
-    class: "3rd",
-    grades: {
-      math: 20,
-      physics: 20,
-      programming: 20,
-      english: 20,
-    },
-  },
+//   {
+//     name: "Ahmed",
+//     class: "3rd",
+//     grades: {
+//       math: 20,
+//       physics: 20,
+//       programming: 20,
+//       english: 20,
+//     },
+//   },
 
-  {
-    name: "Elgoker",
-    class: "3rd",
-    grades: {
-      math: 23,
-      physics: 25,
-      programming: 19,
-      english: 22,
-    },
-  },
-];
+//   {
+//     name: "Elgoker",
+//     class: "3rd",
+//     grades: {
+//       math: 23,
+//       physics: 25,
+//       programming: 19,
+//       english: 22,
+//     },
+//   },
+// ];
 
-const getLeaderBoard = (students) => {
-  let getLeaderBoard = students;
-  return getLeaderBoard
-    .map((student) => {
-      const totalGrades = Object.values(student.grades).reduce(
-        (acc, grade) => acc + grade,
-        0
-      );
-      return { ...student, totalGrades };
-    })
-    .filter((student) => student.totalGrades >= 85)
-    .sort((a, b) => b.totalGrades - a.totalGrades);
+// const getLeaderBoard = (students) => {
+//   let getLeaderBoard = students;
+//   return getLeaderBoard
+//     .map((student) => {
+//       const totalGrades = Object.values(student.grades).reduce(
+//         (acc, grade) => acc + grade,
+//         0
+//       );
+//       return { ...student, totalGrades };
+//     })
+//     .filter((student) => student.totalGrades >= 85)
+//     .sort((a, b) => b.totalGrades - a.totalGrades);
+// };
+
+// console.log(getLeaderBoard(students));
+
+// Coding Challenge #1
+
+/* 
+We're building a football betting app (soccer for my American friends 😅)!
+
+Suppose we get data from a web service about a certain game (below). In this challenge we're gonna work with the data. So here are your tasks:
+
+1. Create one player array for each team (variables 'players1' and 'players2')
+2. The first player in any player array is the goalkeeper and the others are field players. For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10 field players
+3. Create an array 'allPlayers' containing all players of both teams (22 players)
+4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
+5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
+6. Write a function ('printGoals') that receives an arbitrary number of player names (NOT an array) and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in)
+7. The team with the lower odd is more likely to win. Print to the console which team is more likely to win, WITHOUT using an if/else statement or the ternary operator.
+
+TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Then, call the function again with players from game.scored
+
+GOOD LUCK 😀
+*/
+
+const game = {
+  team1: "Bayern Munich",
+  team2: "Borrussia Dortmund",
+  players: [
+    [
+      "Neuer",
+      "Pavard",
+      "Martinez",
+      "Alaba",
+      "Davies",
+      "Kimmich",
+      "Goretzka",
+      "Coman",
+      "Muller",
+      "Gnarby",
+      "Lewandowski",
+    ],
+    [
+      "Burki",
+      "Schulz",
+      "Hummels",
+      "Akanji",
+      "Hakimi",
+      "Weigl",
+      "Witsel",
+      "Hazard",
+      "Brandt",
+      "Sancho",
+      "Gotze",
+    ],
+  ],
+  score: "4:0",
+  scored: ["Lewandowski", "Gnarby", "Lewandowski", "Hummels"],
+  date: "Nov 9th, 2037",
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
 };
 
-console.log(getLeaderBoard(students));
+const [players1, players2] = game.players;
+
+const [gk, ...fieldPlayers] = players1; // Goalkeeper and field players for team 1
+console.log(gk, fieldPlayers);
+
+const allPlayers = [...players1, ...players2]; // All players from both teams
+console.log(allPlayers);
+
+const players1Final = [...players1, "Thiago", "Coutinho", "Perisic"]; // Team 1 with substitutes
+console.log(players1Final);
+
+const { team1, x: draw, team2 } = game.odds; // Odds for each team
+console.log(team1, draw, team2);
+function printGoals(...players) {
+  console.log(`Goals Scored By: ${players.join(", ")}`);
+  console.log(`Total Goals: ${players.length}`);
+}
+
+printGoals("Davies", "Muller", "Lewandowski", "Kimmich");
+printGoals(...game.scored);
+
+const odds = Object.values(game.odds);
+const minOdd = Math.min(...odds);
+const winningTeam = Object.keys(game.odds).find(
+  (team) => game.odds[team] === minOdd
+);
+console.log(`Team more likely to win: ${winningTeam}`); // Team with the lowest odds
